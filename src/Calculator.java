9c6650e8b627ccff911e7b0a650f93a372df5d55
iCanOzcan <123066429+iCanOzcan@users.noreply.github.com>
2023-03-29 09:01:06 +0300

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Calculator {
 
-    public static void main(String args[]){
+    public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         int n1,n2,secim;
 
